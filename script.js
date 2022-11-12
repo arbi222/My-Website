00d99095c6c7ded6066366f4f29280e67518f28d
arbi222 <arbihamolli22@gmail.com>
2022-11-12 17:10:49 +0100

    adding all files

---
+++ script.js
@@ -0,0 +1,29 @@
+let theme = localStorage.getItem('theme')
+
+if(theme == "light"){
+  setTheme("light")
+}
+else{
+  setTheme("dark")
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i = 0 ; themeDots.length > i ; i++){
+      themeDots[i].addEventListener('click' , function(){
+        let mode = this.dataset.mode
+        setTheme(mode)
+      })
+}
+
+function setTheme(mode){
+    if(mode == 'light'){
+      document.getElementById('theme-style').href = './assets/styles/styles.css'
+    }
+
+    if(mode == 'dark'){
+      document.getElementById('theme-style').href = './assets/styles/black.css'
+    }
+
+    localStorage.setItem('theme' , mode)
+}
